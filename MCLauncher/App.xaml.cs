837e7faade6a98f0ffb77c9069123986b24fa731
MrARM <mcmrarm@gmail.com>
2019-03-22 15:29:17 +0100

    Log to Log.txt

--- MCLauncher/App.xaml.cs
+++ MCLauncher/App.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
+using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
@@ -11,5 +12,12 @@ namespace MCLauncher {
     /// Interaction logic for App.xaml
     /// </summary>
     public partial class App : Application {
+
+        protected override void OnStartup(StartupEventArgs e) {
+            base.OnStartup(e);
+            Debug.Listeners.Add(new TextWriterTraceListener("Log.txt"));
+            Debug.AutoFlush = true;
+        }
+
     }
 }
