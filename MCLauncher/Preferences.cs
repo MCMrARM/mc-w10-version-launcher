b07e6e9b67d4a73071d435a8414640dccc87bb3b
Dylan K. Taylor <odigiman@gmail.com>
2019-07-08 13:26:26 +0100

    added missing file

---
+++ MCLauncher/Preferences.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MCLauncher
+{
+	public class Preferences
+	{
+		public bool ShowBetas { get; set; } = true;
+	}
+}
