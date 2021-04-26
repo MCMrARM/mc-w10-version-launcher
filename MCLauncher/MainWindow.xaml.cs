a9f7d14a228f00eb049c675fad4f3e82de62bb0f
Dylan K. Taylor <dktapps@pmmp.io>
2021-04-27 00:12:35 +0100

    Improved backup & restore of package data

--- MCLauncher/MainWindow.xaml.cs
+++ MCLauncher/MainWindow.xaml.cs
@@ -164,7 +164,7 @@ namespace MCLauncher {
             return tmpDir;
         }
 
-        private void BackupMinecraftDataForRemoval() {
+        private void BackupMinecraftDataForRemoval(out string backupPath, out string targetPath) {
             var data = ApplicationDataManager.CreateForPackageFamily(MINECRAFT_PACKAGE_FAMILY);
             string tmpDir = GetBackupMinecraftDataDir();
             if (Directory.Exists(tmpDir)) {
@@ -173,46 +173,30 @@ namespace MCLauncher {
                 MessageBox.Show("The temporary directory for backing up MC data already exists. This probably means that we failed last time backing up the data. Please back the directory up manually.");
                 throw new Exception("Temporary dir exists");
             }
-            Debug.WriteLine("Moving Minecraft data to: " + tmpDir);
-            Directory.Move(data.LocalFolder.Path, tmpDir);
+            backupPath = tmpDir;
+            targetPath = data.LocalFolder.Path;
+            Debug.WriteLine("Moving Minecraft data to: " + backupPath);
+            Directory.Move(data.LocalFolder.Path, backupPath);
         }
 
-        private void RestoreMove(string from, string to) {
-            foreach (var f in Directory.EnumerateFiles(from)) {
-                string ft = Path.Combine(to, Path.GetFileName(f));
-                if (File.Exists(ft)) {
-                    if (MessageBox.Show("The file " + ft + " already exists in the destination.\nDo you want to replace it? The old file will be lost otherwise.", "Restoring data directory from previous installation", MessageBoxButton.YesNo) != MessageBoxResult.Yes)
-                        continue;
-                    File.Delete(ft);
-                }
-                File.Move(f, ft);
-            }
-            foreach (var f in Directory.EnumerateDirectories(from)) {
-                string tp = Path.Combine(to, Path.GetFileName(f));
-                if (!Directory.Exists(tp)) {
-                    if (File.Exists(tp) && MessageBox.Show("The file " + tp + " is not a directory. Do you want to remove it? The data from the old directory will be lost otherwise.", "Restoring data directory from previous installation", MessageBoxButton.YesNo) != MessageBoxResult.Yes)
-                        continue;
-                    Directory.CreateDirectory(tp);
-                }
-                RestoreMove(f, tp);
-            }
-        }
-
-        private void RestoreMinecraftDataFromReinstall() {
-            string tmpDir = GetBackupMinecraftDataDir();
-            if (!Directory.Exists(tmpDir))
+        private void RestoreMinecraftDataAfterRemoval(string backupPath, string targetPath) {
+            if (!Directory.Exists(backupPath))
                 return;
-            var data = ApplicationDataManager.CreateForPackageFamily(MINECRAFT_PACKAGE_FAMILY);
-            Debug.WriteLine("Moving backup Minecraft data to: " + data.LocalFolder.Path);
-            RestoreMove(tmpDir, data.LocalFolder.Path);
-            Directory.Delete(tmpDir, true);
+            Debug.WriteLine("Moving backup Minecraft data to: " + targetPath);
+            string parent = Path.GetDirectoryName(targetPath);
+            if (!Directory.Exists(parent)) {
+                Directory.CreateDirectory(parent);
+            }
+            Directory.Move(backupPath, targetPath);
         }
 
         private async Task RemovePackage(Package pkg) {
             Debug.WriteLine("Removing package: " + pkg.Id.FullName);
             if (!pkg.IsDevelopmentMode) {
-                BackupMinecraftDataForRemoval();
+                string backupPath, targetPath;
+                BackupMinecraftDataForRemoval(out backupPath, out targetPath);
                 await DeploymentProgressWrapper(new PackageManager().RemovePackageAsync(pkg.Id.FullName, 0));
+                RestoreMinecraftDataAfterRemoval(backupPath, targetPath);
             } else {
                 Debug.WriteLine("Package is in development mode");
                 await DeploymentProgressWrapper(new PackageManager().RemovePackageAsync(pkg.Id.FullName, RemovalOptions.PreserveApplicationData));
@@ -250,7 +234,6 @@ namespace MCLauncher {
             string manifestPath = Path.Combine(gameDir, "AppxManifest.xml");
             await DeploymentProgressWrapper(new PackageManager().RegisterPackageAsync(new Uri(manifestPath), null, DeploymentOptions.DevelopmentMode));
             Debug.WriteLine("App re-register done!");
-            RestoreMinecraftDataFromReinstall();
         }
 
         private void InvokeDownload(Version v) {
