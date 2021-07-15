c15541dcbff41606e71628b3704d9fbcc1f8bcb7
Dylan K. Taylor <dktapps@pmmp.io>
2021-07-15 02:09:42 +0100

    Added SampleClasses for UI designer

---
+++ MCLauncher/SampleData/SampleClasses.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MCLauncher.SampleData
+{
+    public class SampleVersion
+    {
+        public string Name { get; set; }
+
+        public string DisplayName { get; set; }
+        public string DisplayInstallStatus { get; set; }
+
+        public bool IsInstalled { get; set; }
+
+        public bool IsBeta { get; set; }
+    }
+
+    public class SampleVersionList : List<SampleVersion> { }
+}
